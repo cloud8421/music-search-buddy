aea0635d625e41cf39a185bcb84871cd78c33d83
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2017-01-06 19:15:32 +0000

    Display search bar and results

--- src/View.elm
+++ src/View.elm
@@ -1,10 +1,38 @@
 module View exposing (..)
 
-import Html exposing (div, text, Html)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
 import Types exposing (..)
+
+
+albumItem : Album -> Html Msg
+albumItem album =
+    li [] [ text album.title ]
+
+
+albumList : List Album -> Html Msg
+albumList albums =
+    ul []
+        (List.map albumItem albums)
+
+
+searchBox : Maybe String -> Html Msg
+searchBox q =
+    input
+        [ type_ "search"
+        , id "q"
+        , name "q"
+        , onInput Search
+        , value (Maybe.withDefault "" q)
+        , placeholder "e.g. Porcupine Tree"
+        ]
+        []
 
 
 root : Model -> Html Msg
 root model =
     div []
-        [ model |> toString |> text ]
+        [ nav [] [ searchBox model.query ]
+        , albumList model.albums
+        ]
