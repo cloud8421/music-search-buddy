7869a053ee40124e0de2f766124b45448f7042ab
Claudio Ortolina <claudio@fullyforged.com>
2017-01-13 16:20:29 +0000

    Adds basic offline support via sw

--- Makefile
+++ Makefile
@@ -1,10 +1,16 @@
-CUSTOM_COMPILE_TARGETS := build/CNAME
-CUSTOM_DIST_TARGETS := dist/CNAME
+CUSTOM_COMPILE_TARGETS := build/CNAME build/sw.js
+CUSTOM_DIST_TARGETS := dist/CNAME dist/sw.js
 
 include elm.mk
 
 build/CNAME dist/CNAME: CNAME
 	cp $< $@
+
+build/sw.js: src/sw.js
+	cp $< $@
+
+$(DIST_FOLDER)/sw.js: src/sw.js $(NODE_BIN_DIRECTORY)/uglifyjs
+	$(NODE_BIN_DIRECTORY)/uglifyjs --compress --mangle --output $@ -- src/sw.js
 
 deploy: prod
 	surge dist
---
+++ src/sw.js
@@ -0,0 +1,32 @@
+var CACHE_NAME = 'com.fullforged.ms.v1';
+var urlsToCache = [
+  '/',
+  '/boot.js',
+  '/main.js',
+  '/main.css'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.debug('cache open');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
