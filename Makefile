c1bfb7802020c66a8d36a3d9392dee7a4e898121
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2017-01-15 13:03:32 +0000

    Remove service worker

--- Makefile
+++ Makefile
@@ -1,16 +1,10 @@
-CUSTOM_COMPILE_TARGETS := build/CNAME build/sw.js
-CUSTOM_DIST_TARGETS := dist/CNAME dist/sw.js
+CUSTOM_COMPILE_TARGETS := build/CNAME
+CUSTOM_DIST_TARGETS := dist/CNAME
 
 include elm.mk
 
 build/CNAME dist/CNAME: CNAME
 	cp $< $@
-
-build/sw.js: src/sw.js
-	cp $< $@
-
-$(DIST_FOLDER)/sw.js: src/sw.js $(NODE_BIN_DIRECTORY)/uglifyjs
-	$(NODE_BIN_DIRECTORY)/uglifyjs --compress --mangle --output $@ -- src/sw.js
 
 deploy: prod
 	surge dist
--- src/boot.js
+++ src/boot.js
@@ -1,15 +1,3 @@
 window.addEventListener('load', function() {
   var app = Elm.Main.fullscreen();
 });
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.debug('ServiceWorker registration successful with scope: ', registration.scope);
-    }).catch(function(err) {
-      // registration failed :(
-      console.debug('ServiceWorker registration failed: ', err);
-    });
-  });
-}
